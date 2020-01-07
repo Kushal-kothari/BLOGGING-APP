6fb7939f78d88cf388600813ebff65929027662a
Kushal-kothari <kushalkothari285@gmail.com>
2020-01-07 20:56:05 +0530

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import ListView,DetailView
+from django.views.generic.edit  import CreateView, UpdateView,DeleteView
+from .models import Post
+from django.urls import reverse_lazy
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+
+class BlogDetailView(DetailView): # new
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
+
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name =  'post_edit.html'   
+    fields = ['title', 'body']
+
+
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
